bb2519060cd50b51fd4ac87dca6c2e57d5b53118
muratcancicek <muratcancicek0@gmail.com>
2019-07-11 19:18:38 -0700

    Correcting widget_test.dart

--- hbp_with_firebase_mlkit/test/widget_test.dart
+++ hbp_with_firebase_mlkit/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:firebase_face_tracking/main.dart';
+import 'package:hbp_with_firebase_mlkit/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
